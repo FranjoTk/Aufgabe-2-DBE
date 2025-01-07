b5fbb5d1ed578afa7d6bd858a35d55ff9a409bba
FranjoTk <147728115+FranjoTk@users.noreply.github.com>
2025-01-08 00:04:11 +0100

    Update test_runtime.py

--- test_runtime.py
+++ test_runtime.py
@@ -18,17 +18,20 @@ class TestModel(unittest.TestCase):
         cls.X_train, cls.y_train, cls.X_test, cls.y_test = download_and_prepare_data()
     
     def test_predict_accuracy(self):
-        model = train_model(cls.X_train, cls.y_train)
-        y_pred = model.predict(cls.X_test)
-        accuracy = np.mean(y_pred == cls.y_test)
+        model = train_model(self.X_train, self.y_train)
+        y_pred = model.predict(self.X_test)
+        accuracy = np.mean(y_pred == self.y_test)
         print_and_log(f"Test Accuracy: {accuracy * 100:.2f}%\n")
-        cls.assertGreater(accuracy, 0.70, "Accuracy should be greater than 70%")
+        self.assertGreater(accuracy, 0.70, "Accuracy should be greater than 70%")
 
-def test_training_time(self):
-    start_time = time.time()
-    train_model(self.X_train, self.y_train)
-    end_time = time.time()
-    
-    # Neuer Grenzwert für die Laufzeit
-    self.assertLess(end_time - start_time, 10, "Training time should be less than 10 seconds")
-    print_and_log(f"Training time: {end_time - start_time:.5f} sec\n")
+    def test_training_time(self):
+        start_time = time.time()
+        train_model(self.X_train, self.y_train)
+        end_time = time.time()
+        
+        # Neuer Grenzwert für die Laufzeit
+        self.assertLess(end_time - start_time, 10, "Training time should be less than 10 seconds")
+        print_and_log(f"Training time: {end_time - start_time:.5f} sec\n")
+
+if __name__ == "__main__":
+    unittest.main()
